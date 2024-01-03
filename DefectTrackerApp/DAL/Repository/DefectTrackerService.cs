9c31c505ecfecc0c9b3d9be7252ab8b62a122812
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-03 15:04:50 +0530

    Add files via upload

---
+++ DefectTrackerApp/DAL/Repository/DefectTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using DefectTrackerApp.DAL.Interface;
+using DefectTrackerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DefectTrackerApp.DAL.Repository
+{
+    public class DefectTrackerService : IDefectTrackerInterface
+    {
+        private IDefectTrackerRepository _repo;
+        public DefectTrackerService(IDefectTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public int DeleteDefect(int DefectId)
+        {
+            var res= _repo.DeleteDefect(DefectId);
+            return res;
+        }
+
+        public Defect GetDefectByID(int DefectId)
+        {
+            return _repo.GetDefectByID(DefectId);
+        }
+        public void Save()
+        {
+            _repo.Save();
+        }
+
+
+        IEnumerable<Defect> IDefectTrackerInterface.GetDefects()
+        {
+            return _repo.GetDefects();
+        }
+
+        Defect IDefectTrackerInterface.InsertDefect(Defect Defect)
+        {
+            return _repo.InsertDefect(Defect);
+        }
+
+        bool IDefectTrackerInterface.UpdateDefect(Defect Defect)
+        {
+            return _repo.UpdateDefect(Defect);
+        }
+    }
+}
